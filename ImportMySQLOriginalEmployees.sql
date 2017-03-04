267afb471e00a6a513780dd90e6a8745ad63f37b
stefan <stefan@stefanproell.at>
2017-03-04 16:53:37 +0100

    import script

--- ImportMySQLOriginalEmployees.sql
+++ ImportMySQLOriginalEmployees.sql
@@ -1,3 +1,5 @@
+-- The original data is stored in the database called employees
+-- Spring created the new schema called spring_employees
 USE `spring_employees`;
 
 -- Departments
@@ -26,6 +28,7 @@ SELECT `employees`.`emp_no`,
     `employees`.`last_name`
 FROM `employees`.`employees`;
 
+-- Join table 
 INSERT INTO `spring_employees`.`dept_emp`
 (`emp_no`,
 `dept_no`,
@@ -37,6 +40,8 @@ SELECT
     `dept_emp`.`from_date`,
     `dept_emp`.`to_date`
 FROM `employees`.`dept_emp`;
+
+-- Join table 
 
 INSERT INTO `spring_employees`.`dept_manager`
 (
@@ -50,6 +55,8 @@ SELECT `dept_manager`.`emp_no`,
     `dept_manager`.`to_date`
 FROM `employees`.`dept_manager`;
 
+-- Titles
+
 INSERT INTO `spring_employees`.`titles`
 (`emp_no`,
 `title`,
@@ -60,6 +67,8 @@ SELECT `titles`.`emp_no`,
     `titles`.`from_date`,
     `titles`.`to_date`
 FROM `employees`.`titles`;
+
+-- Salaries
 
 INSERT INTO `spring_employees`.`salaries`
 (`emp_no`,
